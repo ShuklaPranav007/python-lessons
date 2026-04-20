2db3790869c71e4345f4ad746c7784a25c04dc4c
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-04-21 00:49:56 +0530

    list n tup

--- list.py
+++ list.py
@@ -1,9 +1,28 @@
-app= ["cluade", "gemini", "chatgpt"]
-print(app)
-app[1]="grok"
-print(app)
+# app= ["cluade", "gemini", "chatgpt"]
+# print(app)
+# app[1]="grok"
+# print(app)
 
-num = [12,43,56,75,84,32,21,11,3,234,436]
-num.reverse()
+# num = [12,43,56,75,84,32,21,11,3,234,436]
+# num.reverse()
 # num2 = num.sort() gives value none
-print(num)
+# print(num)
+
+
+# fruit_list = []
+# for items in range(0,7):
+#     fruit_list.append(input("enter your fruit"))
+
+# print(fruit_list)
+
+
+# marks= []
+# for mark in range(0,6):
+#     marks.append(int(input(" enter marks : ")))
+
+# marks.sort()
+# print(marks)
+# print(marks)
+
+num =[1,2,3,4,5]
+print(sum(num)) 
