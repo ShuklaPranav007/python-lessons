b59bd68d22bbdc19feb4e1b395e919b50473d035
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-04-21 00:30:54 +0530

    list and tuples

---
+++ list.py
@@ -0,0 +1,9 @@
+app= ["cluade", "gemini", "chatgpt"]
+print(app)
+app[1]="grok"
+print(app)
+
+num = [12,43,56,75,84,32,21,11,3,234,436]
+num.reverse()
+# num2 = num.sort() gives value none
+print(num)
