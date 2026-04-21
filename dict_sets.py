9986190d3cb3db0b91211026e0b9e923818f534b
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-04-21 16:45:59 +0530

    dict and sets

---
+++ dict_sets.py
@@ -0,0 +1,47 @@
+# marks={
+#     "pranav":87,
+#     "riya":78,
+#     "mummy":426
+# }
+# # print(marks)
+# # print(type(marks))
+# # print(marks.items())
+
+
+# # empty dict 
+# d = {}
+# # print(type(d))
+
+# s = {12,32,37}
+# emptySet = set()
+# # print(type(s))
+# # print(type(emptySet))
+
+
+# s1 = {12,341,332,56,86}
+
+# s2 ={684,8,32,97,24}
+# print(s1.union(s2))
+
+
+dict_trans= {
+    "hindi1":"eng1",
+    "hindi2":"eng2",
+    "hindi3":"eng3",
+    "hindi4":"eng4",
+    "hindi5":"eng5"
+}
+# print(dict_trans["hindi4"])
+
+# inputSet = set()
+# for items in range (0,8):
+#     inputSet.add(int(input("enter number")))
+
+# print(inputSet)
+
+
+set = set()
+set.add(20)
+set.add(20.00)
+set.add('20')
+print(set)
