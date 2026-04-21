e250b40e0db81262edbf5d20a8f5fc7a17136076
ShuklaPranav007 <shuklapranav739@gmail.com>
2026-04-22 00:39:58 +0530

    conditioinals

---
+++ conditonals.py
@@ -0,0 +1,14 @@
+a, b, c, d = 807, 45, 89, 12
+
+if a >= b and a >= c and a >= d:
+    print(a)
+elif b >= a and b >= c and b >= d:
+    print(b)
+elif c >= a and c >= b and c >= d:
+    print(c)
+else:
+    print(d)
+
+print("hello",a,b,c,d)
+
+
